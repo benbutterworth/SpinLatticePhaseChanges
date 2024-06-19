68a63276614bb3027228aec18dee57a1f370e080
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-06-19 19:11:21 +0100

    create constants.jl
    physics constants needed to run simulation

---
+++ src/constants.jl
@@ -0,0 +1,10 @@
+export μ, ħ, g, κ
+
+# Reduced Plank's constant
+const ħ = 1.0545718E-34 # [J][S]
+# Bohr Magneton
+const μ = 9.274010E-24  #[J]/[K]
+# Electron g-factor
+const g = 2             # []
+# Boltzman constant
+const κ = 1.380649E-23  # [J]/[K]
