b8e2c0132b1b0d43aeedcb9a30356ff0db1e69ad
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-07-13 12:41:35 +0100

    Update measurements.jl
    Naive attempt to avg. direction of spins (WIP - how to avg. polar coods meaninfully is not trivial).

--- src/measurements.jl
+++ src/measurements.jl
@@ -2,8 +2,26 @@
 
 function magnetisation end
 
+"""
+    meanspindirection(sg::SpinGrid) -> Float64
+Return the average direction of all spins in `sg` in radians.
+"""
+function meanspindirection(sg::SpinGrid)
+    xyspingrid = map(spins(sg)) do x
+        convert(XYSpin, x)
+    end |> SpinGrid
+
+    spindirections = map(spin, spins(xyspingrid))
+
+    mean(spindirections)
+end
+
+"""
+    magnetisation(sg::SpinGrid)
+Return 0
+"""
 function magnetisation(sg::SpinGrid)
-    # average direction of all spins
+    0
 end
 
 function entropy end
